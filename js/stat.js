c2529a83188e6200c5a716f7fdaa332db4f802f9
thirtylynx <thirtylynx@gmail.com>
2017-08-15 22:46:50 +0500

    Beautify code, add const

--- js/stat.js
+++ js/stat.js
@@ -11,29 +11,39 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 120, 60);
 
 
-  var getBlueLightness = function () {
-    return 'hsl(' + 240 + ', ' + 100 + '%, ' + Math.round(Math.random() * 100) + '%)';
+  var getRandomBlueColor = function () {
+    return 'hsl(240, 100%, ' + Math.round(Math.random() * 100) + '%)';
   };
-  var histogramHeight = 150;
+  var HISTOGRAM_HEIGHT = 150;
+  var BAR_WIDTH = 40;
+  var INDENT = 90;
+  var BAR_INDENT = 100;
+  var TEXT_INDENT = 20;
+  var INITIAL_X = 150;
+  var INITIAL_Y = 240;
   var maxTime = Math.max.apply(null, times);
-  var step = histogramHeight / (maxTime - 0);
-  var barWidth = 40;
-  var barIndent = 100;
-  var textIndent = 20;
-  var indent = 90;
-  var initialX = 150;
-  var initialY = 240;
+  var step = HISTOGRAM_HEIGHT / maxTime;
 
   ctx.textBaseline = 'top';
   for (var i = 0; i < times.length; i++) {
-    var barHeight = times[i] * (step);
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getBlueLightness();
-    ctx.fillRect(initialX + indent * i, initialY, barWidth, -barHeight);
+    var YOUR_NAME = 'Вы';
+    var YOUR_COLOR = 'rgba(255, 0, 0, 1)';
+    var PLAYERS_COLOR = getRandomBlueColor();
+    var barHeight = times[i] * step;
+    var totalWidth = INITIAL_X + INDENT * i;
+    var playerName = names[i];
+    var playerTime = times[i];
+    var totalNameIndent = HISTOGRAM_HEIGHT + BAR_INDENT;
+    var totalTimeIndent = INITIAL_Y - barHeight - TEXT_INDENT;
+
+
+    ctx.fillStyle = names[i] === YOUR_NAME ? YOUR_COLOR : PLAYERS_COLOR;
+    ctx.fillRect(totalWidth, INITIAL_Y, BAR_WIDTH, -barHeight);
     ctx.fillStyle = 'black';
     ctx.font = '16px Tahoma';
     ctx.textBaseline = 'hanging';
-    ctx.fillText(names[i], initialX + indent * i, histogramHeight + barIndent);
-    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - barHeight - textIndent);
+    ctx.fillText(playerName, totalWidth, totalNameIndent);
+    ctx.fillText(Math.round(playerTime), totalWidth, totalTimeIndent);
   }
 };
 
