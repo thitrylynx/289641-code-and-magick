012c18e819929b3c906a82bf12b09e792c9fc82d
thirtylynx <thirtylynx@gmail.com>
2017-08-15 12:45:18 +0500

    Beautify code

--- js/stat.js
+++ js/stat.js
@@ -10,33 +10,29 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
-  var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
-  var getHSL = function (hue, saturation, lightness) {
-    return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
-  };
 
+  var getBlueLightness = function () {
+    return 'hsl(' + 240 + ', ' + 100 + '%, ' + Math.round(Math.random() * 100) + '%)';
+  };
   var histogramHeight = 130;
-  var step = histogramHeight / (max - 0);
+  var maxTime = Math.max.apply(null, times);
+  var step = histogramHeight / (maxTime - 0);
   var barWidth = 40;
+  var barHeight = 210;
+  var barIndent = 120;
   var indent = 90;
   var initialX = 140;
   var initialY = 240;
 
   ctx.textBaseline = 'top';
   for (var i = 0; i < times.length; i++) {
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getHSL(240, 100, Math.round(Math.random() * 100));
+    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getBlueLightness();
     ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * (-step));
     ctx.fillStyle = 'black';
     ctx.font = '16px Tahoma';
     ctx.textBaseline = 'hanging';
-    ctx.fillText(names[i], initialX + indent * i, histogramHeight + 120);
-    ctx.fillText(Math.round(times[i]), initialX + indent * i, 210 - (times[i] * (step)));
+    ctx.fillText(names[i], initialX + indent * i, histogramHeight + barIndent);
+    ctx.fillText(Math.round(times[i]), initialX + indent * i, barHeight - (times[i] * (step)));
   }
 };
 
